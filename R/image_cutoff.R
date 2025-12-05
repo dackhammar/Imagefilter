a0be2a27b7e6af9b2787c55fa0c25db01c2bab6a
dackhammar <dackhammar@gmail.com>
2025-12-05 23:39:36 +0100

    Edited image_grey to use same iterative approach as image_cutoff. Removed EBImage dependency. Added code to handle rgba.

--- R/image_cutoff.R
+++ R/image_cutoff.R
@@ -17,32 +17,44 @@
 #'
 #' @export
 image_cutoff <- function (x,y) {
-  pic<-image_read(x)
-  bitmap<-pic[[1]]
-  bitmap_cutoff<-bitmap
-  for (i in 1:length(bitmap)) {
-
-    if (i %% 3 == 1){
+  pic<-image_read(x) # Read image
+  bitmap<-pic[[1]] # Extract bitmap
+  # Review notes: Removed the bitmap_cutoff, we can apply changes directly to bitmap.
+  # Added the below so we can handle images with rgba (derived from edit in image_grey)
+  dims<-dim(bitmap)
+  if (dims[1] == 4) {
+    a<-bitmap[4,,,drop = F] # Extract alpha channel
+    bitmap<-bitmap[1:3,,] # Extract
+  }
+  # Iterate over bitmap
+  # Review notes: Changed this to seq by 3 to remove if else.
+  for (i in seq(1, length(bitmap), by = 3)) {
       greyscale<-strtoi(bitmap[i],16)*0.299+strtoi(bitmap[i+1],16)*0.587+strtoi(bitmap[i+2],16)*0.114
+      # Apply cut-off
+      # Review notes: One might consider adding an argument to define the cutoff)
       if (greyscale < 128) {
-        bitmap_cutoff[i] <- as.raw("00")
-        bitmap_cutoff[i+1] <- as.raw("00")
-        bitmap_cutoff[i+2] <- as.raw("00")
+        bitmap[i] <- as.raw(0)
+        bitmap[i+1] <- as.raw(0)
+        bitmap[i+2] <- as.raw(0)
       }
       else {
-        bitmap_cutoff[i] <- as.raw("255")
-        bitmap_cutoff[i+1] <- as.raw("00")
-        bitmap_cutoff[i+2] <- as.raw("255")
+        bitmap[i] <- as.raw(255)
+        bitmap[i+1] <- as.raw(0)
+        bitmap[i+2] <- as.raw(255)
       }
-
-    }
-
-    else {
-      i = i+1
-    }
   }
-  output <- image_read(bitmap_cutoff)
+  # Apply alpha channel back if 4 channel input
+  if (dims[1] == 4) {
+    out <- array(as.raw(0), dim = dims)
+    out[1:3,,] <- bitmap
+    out[4,,] <- a
+    bitmap <- out
+  }
+  # Read image from bitmap
+  output <- image_read(bitmap)
+  # Save image
   image_write(output, y)
+  # Return image
   return(output)
 }
 
--- R/image_grey.R
+++ R/image_grey.R
@@ -9,19 +9,52 @@
 #' @return grey figures
 #'
 #' @importFrom magick image_read
-#' @importFrom EBImage readImage
-#' @importFrom EBImage writeImage
-#' @importFrom EBImage channel
 #'
 #' @examples
 #' image_grey("https://jeroen.github.io/images/frink.png","test.jpg")
 #'
 #' @export
 image_grey <- function (x,y) {
-  image_data <- readImage(x)
-  new_image <- channel(image_data, "green")*0.587 + channel(image_data, "blue")*0.114 + channel(image_data, "red")*0.299
+  # Review notes:
+  # I choose to use your logic from the cutoff function instead of
+  # relying on the functions from EBImage since you already had created
+  # a function for generating grayscale values and modifying the bitmap
+  # image.
+  pic<-magick::image_read(x) # Read image
+  bitmap<-pic[[1]] # Extract bitmap
+  # Review notes:
+  # This was tricky, test image was transparent png with alpha channel.
+  # So we need to remove the alpha channel to use the same approach.
+  # Option would be to vectorize over bitmap[1,,], bitmap[2,,] and, bitmap[3,,]
+  # Check number of channels
+  dims<-dim(bitmap)
+  if (dims[1] == 4) {
+    a<-bitmap[4,,,drop = F] # Extract alpha channel
+    bitmap<-bitmap[1:3,,] # Extract
+  }
+  # Review notes: Removed the bitmap_cutoff, we can apply changes directly to bitmap.
+  # Iterate over bitmap
+  # Review notes: Changed this to seq by 3 to remove if else.
+  for (i in seq(1, length(bitmap), by = 3)) {
+      # Calculate grayscale value
+      greyscale <- strtoi(bitmap[i],16)*0.299+strtoi(bitmap[i+1],16)*0.587+strtoi(bitmap[i+2],16)*0.114
+      # Apply to all channels
+      bitmap[i] <- as.raw(greyscale)
+      bitmap[i+1] <- as.raw(greyscale)
+      bitmap[i+2] <- as.raw(greyscale)
+  }
+  # Apply alpha channel back if 4 channel input
+  if (dims[1] == 4) {
+    out <- array(as.raw(0), dim = dims)
+    out[1:3,,] <- bitmap
+    out[4,,] <- a
+    bitmap <- out
+  }
 
-  writeImage(new_image, y)
-  return(image_read(new_image))
+  # Read image from bitmap
+  output <- magick::image_read(bitmap)
+  # Save file
+  magick::image_write(output, y)
+  # Return image
+  return(output)
 }
-
